9af79fc00bdbe78ad02475602e03348c310bb1be
yanis21 <khalfaouianis3100@gmail.com>
2023-03-11 12:29:13 +0100

    myapp

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainComponent } from './main/main.component';
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
+import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
+import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    SalesByMonthComponent,
+    SalesByCategoryComponent,
+    LastFewTransactionsComponent,
+    TopThreeProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    ChartModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {
+  faDashboard,
+  faLocation,
+  faShop,
+  faBox,
+  faMoneyBill,
+  faChartBar,
+  faContactBook,
+  faHand,
+  faCoffee,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss']
+})
+export class SideNavComponent {
+  faCoffee = faCoffee;
+  faDashboard = faDashboard;
+  faLocation = faLocation;
+  faShop = faShop;
+  faBox = faBox;
+  faMoneyBill = faMoneyBill;
+  faChartBar = faChartBar;
+  faContactBook = faContactBook;
+  faHand = faHand;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
